02c1230f0ae3053ef9f072f0a0c0bd0176715730
Mur1thi <kinotikevin@gmail.com>
2023-12-24 11:38:32 +0300

    Sun 24th Dec 2023

--- app.py
+++ app.py
@@ -1,149 +1,52 @@
+from flask import Flask, render_template, redirect, url_for, request, session
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template, redirect, url_for, request, flash
-from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
+from models import User, error, Fundraiser, Contribution
 from werkzeug.security import generate_password_hash, check_password_hash
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yoursecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Toa.db'
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 Bootstrap(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Toa.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-# Import models
-from models import User, Fundraiser, Contribution
-
-# index route
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
-# Login route
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        user = User.query.filter_by(username=request.form['username']).first()
+        if not user:
+            return error("must provide username", 403)
 
-        user = User.query.filter_by(username=username).first()
-
-        if user and user.check_password(password):
-            login_user(user)
-            return redirect(url_for('dashboard'))
-        else:
-            return 'Invalid credentials'
+        if user and check_password_hash(user.password, request.form['password']):
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
+        if not user:
+                return error("must provide password", 403)
 
     return render_template('login.html')
 
-@app.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html', user=current_user)
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()  # Logout logic
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
 
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
 
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    if current_user.is_authenticated:
-        return render_template('dashboard.html')
-    else:
         return redirect(url_for('login'))
 
+    return render_template('register.html')
 
-@app.route('/fundraisers/new', methods=['GET', 'POST'])
-@login_required
-def create_fundraiser():
-    if request.method == 'POST':
-        name = request.form['name']
-        description = request.form['description']
-        goal = request.form['goal']
-
-        fundraiser = Fundraiser(name=name, description=description, goal=goal, user=current_user)
-
-        db.session.add(fundraiser)
-        db.session.commit()
-
-        return redirect('/dashboard')
-
-    return render_template('create_fundraiser.html')
-
-@app.route('/my_fundraisers')
-@login_required
-def my_fundraisers():
-    fundraisers = Fundraiser.query.filter_by(user_id=current_user.id).all()
-    return render_template('my_fundraisers.html', fundraisers=fundraisers)
-
-@app.route('/fundraiser/<int:id>')
-@login_required
-def fundraiser(id):
-    fundraiser = Fundraiser.query.get_or_404(id)
-    if fundraiser.user_id != current_user.id:
-        flash('Not authorized')
-        return redirect('/dashboard')
-    return render_template('fundraiser.html', fundraiser=fundraiser)
-
-
-@app.route('/fundraiser/<int:id>/edit', methods=['GET', 'POST'])
-@login_required
-def edit_fundraiser(id):
-    fundraiser = Fundraiser.query.get(id)
-    if fundraiser.user_id != current_user.id:
-        flash('Not authorized')
-        return redirect(url_for('dashboard'))
-
-    # Allow edit
-
-@app.route('/contribute', methods=['GET', 'POST'])
-@login_required
-def contribute():
-    # Check if user already has active fundraiser
-    current_fundraiser = Fundraiser.query.filter_by(user_id=current_user.id, end_date=None).first()
-    if current_fundraiser:
-        flash('You already have an active fundraiser')
-        return redirect(url_for('dashboard'))
-
-    # Get fundraiser to contribute to
-    fundraiser_id = request.form.get('fundraiser_id')
-    fundraiser = Fundraiser.query.get(fundraiser_id)
-    if not fundraiser:
-        flash('Invalid fundraiser')
-        return redirect(url_for('dashboard'))
-
-    # Create new contribution
-    if request.method == 'POST':
-        amount = request.form['amount']
-        date = request.form['date']
-
-        contribution = Contribution(
-            amount=amount,
-            date=date,
-            fundraiser=fundraiser,
-            user=current_user
-        )
-
-        db.session.add(contribution)
-        db.session.commit()
-
-        flash('Contribution added!')
-
-    return render_template('contribute.html', fundraiser=fundraiser)
-
-
-# Other routes and views
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,33 +1,53 @@
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(120))
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
 
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
+    def __repr__(self):
+        return f'<User {self.username}>'
 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+# from https://github.com/jacebrowning/memegen#special-characters
+def error(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("error.html", top=code, bottom=escape(message)), code
+
 
 class Fundraiser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    name = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String, nullable=False)
     description = db.Column(db.String)
-    goal = db.Column(db.Integer)
-    current = db.Column(db.Integer)
-    end_date = db.Column(db.DateTime)
-    image = db.Column(db.String)
+
+    user = db.relationship('User', backref=db.backref('fundraisers', lazy=True))
+
+    def __repr__(self):
+        return f'<Fundraiser {self.name}>'
 
 
 class Contribution(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fundraiser_id = db.Column(db.Integer, db.ForeignKey('fundraisers.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    amount = db.Column(db.Integer)
-    date = db.Column(db.DateTime)
+    fundraiser_id = db.Column(db.Integer, db.ForeignKey('fundraiser.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+
+    fundraiser = db.relationship('Fundraiser', backref=db.backref('contributions', lazy=True))
+    user = db.relationship('User', backref=db.backref('contributions', lazy=True))
+
+    def __repr__(self):
+        return f'<Contribution ${self.amount} to {self.fundraiser.name}>'
