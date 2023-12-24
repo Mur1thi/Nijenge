df8fbb49bc3b8a5ca9d40220b2612c1f29d233ff
Mur1thi <kinotikevin@gmail.com>
2023-12-24 14:29:42 +0300

    Sun 24th Dec 2023 User Login logic and front end complete

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from models import User, error, Fundraiser, Contribution
 from werkzeug.security import generate_password_hash, check_password_hash
+from models import db
 
 app = Flask(__name__)
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Toa.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///toa.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
+# Initialize SQLAlchemy with Flask app
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
@@ -20,18 +24,23 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = User.query.filter_by(username=request.form['username']).first()
-        if not user:
-            return error("must provide username", 403)
+        username = request.form['username']
+        password = request.form['password']
 
-        if user and check_password_hash(user.password, request.form['password']):
+        try:
+            user = User.query.filter_by(username=username).first()
+
+            if not user or not check_password_hash(user.password, password):
+                return error("User not found", 403)
+
             session['user_id'] = user.id
             return redirect(url_for('index'))
-        if not user:
-                return error("must provide password", 403)
+
+        except Exception as e:
+            # catch any other exceptions
+            return error(str(e), 500)
 
     return render_template('login.html')
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -46,7 +55,6 @@ def register():
         return redirect(url_for('login'))
 
     return render_template('register.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
