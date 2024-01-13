98897d0c7c9c91f2b9b56790797465339b97c942
Mur1thi <kinotikevin@gmail.com>
2024-01-13 14:19:33 +0300

    Formatted app.py and other files

--- static/js/Toastr.js
+++ static/js/Toastr.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const updateButton = document.getElementById('update-button');
-  const messageInput = document.getElementById('message');
-  const fundraiserIdInput = document.getElementById('fundraiserId');
+document.addEventListener("DOMContentLoaded", function () {
+  const updateButton = document.getElementById("update-button");
+  const messageInput = document.getElementById("message");
+  const fundraiserIdInput = document.getElementById("fundraiserId");
 
-  updateButton.addEventListener('click', function (event) {
-    event.preventDefault();  // Prevent the form from submitting normally
+  updateButton.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the form from submitting normally
 
     const message = messageInput.value;
     const fundraiserId = fundraiserIdInput.value;
@@ -13,19 +13,19 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   function saveContribution(fundraiserId, message) {
-    $.post('/fundraiser_success/' + fundraiserId, {
-      message: message
+    $.post("/fundraiser_success/" + fundraiserId, {
+      message: message,
     })
-    .done(function(response) {
-      if (response.status === 'success') {
-        toastr.success(response.message);
-        $('#message').val(''); // Clear the text area
-      } else {
-        toastr.error(response.message);
-      }
-    })
-    .fail(function(xhr, status, error) {
-      toastr.error('Error saving contribution');
-    });
+      .done(function (response) {
+        if (response.status === "success") {
+          toastr.success(response.message);
+          $("#message").val(""); // Clear the text area
+        } else {
+          toastr.error(response.message);
+        }
+      })
+      .fail(function (xhr, status, error) {
+        toastr.error("Error saving contribution");
+      });
   }
 });
